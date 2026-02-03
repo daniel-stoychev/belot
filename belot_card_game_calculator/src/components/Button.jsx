5c8854f239eb7635b0030e8f0d7e0cd983e6c72f
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-02-03 21:20:39 +0200

    update UI for scores & add Button component and styling

--- belot_card_game_calculator/src/components/Button.jsx
+++ belot_card_game_calculator/src/components/Button.jsx
@@ -1,7 +1,5 @@
-export default function Button() {
-  return (
-    <>
-      <h1>This is a button</h1>
-    </>
-  );
+import styles from "./Button.module.css";
+
+export default function Button({ children }) {
+  return <button className={styles.button}>{children}</button>;
 }
--- belot_card_game_calculator/src/components/ScoreBoard.jsx
+++ belot_card_game_calculator/src/components/ScoreBoard.jsx
@@ -1,7 +1,39 @@
+import Button from "./Button.jsx";
+import styles from "./ScoreBoard.module.css";
+
 export default function ScoreBoard() {
+  const teamScores = (e) => {
+    e.preventDefault();
+  };
   return (
-    <>
-      <h1>ScoreBoard</h1>
-    </>
+    <section className={styles.container}>
+      <h1 className={styles.title}>Score Board</h1>
+
+      <div className={styles.board}>
+        <div className={styles.column}>
+          {/* Team A */}
+          <h2 className={styles.teamTitle}>Team A</h2>
+
+          <input
+            type="number"
+            name="teamA"
+            placeholder="Enter score"
+            className={styles.input}
+          />
+
+          {/* Team B */}
+          <h2 className={styles.teamTitle}>Team B</h2>
+
+          <input
+            type="number"
+            name="teamB"
+            placeholder="Enter score"
+            className={styles.input}
+          />
+
+          <Button>Calc</Button>
+        </div>
+      </div>
+    </section>
   );
 }
